2261f264b1b6f37f9c14c9cd309f03d174dd55b9
Divya Jayendra Chaudhari <69595514+Divya-105@users.noreply.github.com>
2022-11-21 23:21:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
 
     st.write(font_css, unsafe_allow_html=True)
 
-    tabs = st.tabs(('Panorama Image'))
+    #tabs = st.tabs(('Panorama Image Created By Divya Chaudhari(Roll No 227)'))
 
     with tabs[0]:
         panorama()
@@ -64,7 +64,7 @@ def uploadImages(key):
 
 def panorama():
 
-    st.header("Panorama Image")
+    st.header("Panorama Image Created By Divya Chaudhari(Roll No 227)")
 
     imgs = uploadImages(0)
 
